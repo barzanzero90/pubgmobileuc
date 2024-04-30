f3139ba147b7223912194d087a60655b8f84d756
Barzan <99289946+barzanzero90@users.noreply.github.com>
2024-04-30 10:40:40 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        "md" : "900px",
+        "sm" : "500px",
+      }
+    },
+  },
+  plugins: [],
+}
+
